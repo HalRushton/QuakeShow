25c0bdd3a4ab11745eb5a284b7bea0c445390a45
HalRushton <digbads@gmail.com>
2022-05-30 13:54:52 -0600

    User Key

---
+++ QuakeShow/UerKey.swift
@@ -0,0 +1,14 @@
+//
+//  UserKey.swift
+//  Quake
+//
+//  Created by Hal Jay Rushton on 5/30/22.
+//
+
+import Foundation
+
+enum UserKey {
+    enum Bool: String {
+        case hasSetupDatabase
+    }
+}
